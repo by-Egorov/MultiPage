ed192f6fe98b09f5ecb0155d205358688baafc76
Alexander <egorov.dev@gmail.com>
2022-08-18 01:10:32 +0300

    Added choice of plan for 6 and 12 months

--- js/common.js
+++ js/common.js
@@ -4,10 +4,14 @@ const dropBtn = document.querySelector('.dropbtn')
 const modalLang = document.querySelector('.dropdown-content-lang')
 
 const faqItem = document.querySelector('.faq-menu__list')
-const monthsBtn = document.querySelector('.chose-plan__button-months')
+const sixMonthBtn = document.querySelector('.btn-months')
+const twelveMonthsBtn = document.querySelector('.btn-months-choise')
+const planCard = document.querySelector('.chose-plan-card')
 
+// Menu CLick
+menuLink.addEventListener('click', menuClick)
 
-menuLink.addEventListener('click', () => {
+function menuClick() {
 	menuLink.classList.toggle('active')
 
 	if (menuLink.classList.contains('active')) {
@@ -15,10 +19,13 @@ menuLink.addEventListener('click', () => {
 	} else {
 		modal.classList.remove('active')
 	}
-})
+}
+// Menu CLick
 
+// Menu Lang
+dropBtn.addEventListener('click', btnClick)
 
-dropBtn.addEventListener('click', () => {
+function btnClick() {
 	dropBtn.classList.toggle('active')
 
 	if (dropBtn.classList.contains('active')) {
@@ -26,7 +33,8 @@ dropBtn.addEventListener('click', () => {
 	} else {
 		modalLang.classList.remove('active')
 	}
-})
+}
+// Menu Lang
 
 faqItem.addEventListener('click', (event) => {
 	const target = event.target
@@ -34,10 +42,30 @@ faqItem.addEventListener('click', (event) => {
 	target.classList.toggle('active')
 })
 
+// Monts btn
+twelveMonthsBtn.addEventListener('click', twelveMonths)
+sixMonthBtn.addEventListener('click', sixMonth)
 
-monthsBtn.addEventListener('click', (event) => {
-	const target = event.target
+function twelveMonths() {
+	twelveMonthsBtn.classList.add('active')
+	sixMonthBtn.classList.remove('active')
 
-	target.classList.toggle('active')
-})
+	if (twelveMonthsBtn.classList.contains('active')) {
+		planCard.classList.add('active')
+	} else {
+		planCard.classList.remove('active')
+	}
+}
+
+function sixMonth() {
+	sixMonthBtn.classList.add('active')
+	twelveMonthsBtn.classList.remove('active')
+
+	if (sixMonthBtn.classList.contains('active')) {
+		planCard.classList.remove('active')
+	} else {
+		planCard.classList.add('active')
+	}
+}
+// Monts btn
 
